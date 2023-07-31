5603f8d848d93b537a5a576752a6b0bba2c2cb53
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2023-07-31 09:42:55 +0545

    DetailedCharacterScreen Created

---
+++ app/src/main/java/com/example/rickandmorty/presentation/DetailedCharacterScreen.kt
@@ -0,0 +1,33 @@
+package com.example.rickandmorty.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun DetailedCharacterScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Name", fontSize = 30.sp, fontWeight = FontWeight.SemiBold)
+        }
+    }
+}
